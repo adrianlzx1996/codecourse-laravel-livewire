f3925959d19dcccdac78833e544360e2c6998955
Adrian Leong <leongadrian36@gmail.com>
2022-08-07 21:11:24 +0800

    style

--- resources/views/livewire/post-create.blade.php
+++ resources/views/livewire/post-create.blade.php
@@ -1,9 +1,8 @@
 <div class="mb-4">
 	<form wire:submit.prevent="addPost">
 		<div class="mb-3">
-			<label for="body" class="sr-only">Your post</label>
+			<label for="body" class="visually-hidden">Your post</label>
 			<textarea name="body" id="body" class="form-control" wire:model.debounce="body"></textarea>
-			{{ $body }}
 		</div>
 
 		<button type="submit" class="btn btn-primary">Post</button>
