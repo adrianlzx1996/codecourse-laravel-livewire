839134885449fd91265638c59f2510fff9c843a4
Adrian Leong <leongadrian36@gmail.com>
2022-08-07 17:27:35 +0800

    livewire PostSingle

---
+++ app/Http/Livewire/PostSingle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PostSingle extends Component
+{
+    public function render()
+    {
+        return view('livewire.post-single');
+    }
+}
